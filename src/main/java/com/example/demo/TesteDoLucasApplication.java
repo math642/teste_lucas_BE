bf0b755a0ba63ae1749cf3a483f2b70390873be1
Matheus <matheuscavalcante642@gmail.com>
2023-02-04 18:54:48 -0300

    organizando e começando projeto maven para spring

---
+++ src/main/java/com/example/demo/TesteDoLucasApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteDoLucasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteDoLucasApplication.class, args);
+	}
+
+}
