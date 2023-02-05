3cf4f56fd2b58e40e098fd945a2de710f252cf48
Matheus <matheuscavalcante642@gmail.com>
2023-02-05 18:49:24 -0300

    Criado Repositories e os Controllers

---
+++ src/main/java/com/testelucas/demo/repositories/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package com.testelucas.demo.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.testelucas.demo.entities.Usuario;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    
+}
