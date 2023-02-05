2af0d7ddd85e2d712aeff8947977515efd14d526
Matheus <matheuscavalcante642@gmail.com>
2023-02-05 15:39:54 -0300

    Pastas organizadas e resolvido erro de ter posto GeneretedV no Login

--- src/main/java/com/testelucas/demo/entities/Plano.java
+++ src/main/java/com/testelucas/demo/entities/Plano.java
@@ -1,7 +1,5 @@
 package com.testelucas.demo.entities;
 
-import com.fasterxml.jackson.databind.annotation.JsonAppend.Prop;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -38,6 +36,41 @@ public class Plano {
     }
     public void setValor(Boolean valor) {
         this.valor = valor;
+    }
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + ((valor == null) ? 0 : valor.hashCode());
+        return result;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Plano other = (Plano) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        if (valor == null) {
+            if (other.valor != null)
+                return false;
+        } else if (!valor.equals(other.valor))
+            return false;
+        return true;
     }
     
 }
--- src/main/java/com/testelucas/demo/entities/Usuario.java
+++ src/main/java/com/testelucas/demo/entities/Usuario.java
@@ -1,39 +1,42 @@
 package com.testelucas.demo.entities;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "td_usuario")
 public class Usuario {
-    
+
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String login;
     private String nome;
     private String senha;
 
-    public Usuario(){
+    public Usuario() {
 
     }
+
     public String getLogin() {
         return login;
     }
+
     public void setLogin(String login) {
         this.login = login;
     }
+
     public String getNome() {
         return nome;
     }
+
     public void setNome(String nome) {
         this.nome = nome;
     }
+
     public String getSenha() {
         return senha;
     }
+
     public void setSenha(String senha) {
         this.senha = senha;
     }
